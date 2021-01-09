17dc7817423d97323ac0f99120ded748483d2701
bubblecfd <taoyeh@gmail.com>
2021-01-08 22:00:38 -0800

    Lesson 01 assignment

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
+from peewee import fn
 
-from model import Donation 
+from model import Donor, Donation
 
 app = Flask(__name__)
 
@@ -15,7 +16,21 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create_donation():
+    if request.method == 'GET':
+        return render_template('create_donation.jinja2')
     
+    if request.method == 'POST':
+        try:
+            #name = Donor.select().where(fn.Lower(Donor.name)== request.form['name'].lower()).get()
+            name = Donor.get(fn.Lower(Donor.name) == request.form['name'].lower())
+            Donation(donor=name, value=int(request.form['amount'])).save()
+            return redirect(url_for('home'))
+        except:
+            return render_template('create_donation.jinja2', error="Donor does not exist.")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
